459b50c2972dad495bb0776c4d7945eddc0c86b8
Denis_Galieuski <Denis_Galieuski@epam.com>
2018-04-03 11:17:07 +0300

    Tests moved to "main" package

--- src/main/java/by/epam/viacom/cc/tests/MainRunner.java
+++ src/main/java/by/epam/viacom/cc/tests/MainRunner.java
@@ -1,23 +1,25 @@
 package by.epam.viacom.cc.tests;
 
+
 import org.testng.TestNG;
 import org.testng.xml.SuiteXmlParser;
 import org.testng.xml.XmlSuite;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class MainRunner {
-
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String file = "testng.xml";
         SuiteXmlParser parser = new SuiteXmlParser();
-        XmlSuite suite = parser.parse(file, new FileInputStream(file), true);
+        XmlSuite suite = null;
+        try {
+            suite = parser.parse(file, new FileInputStream(file), true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         List<XmlSuite> suitesList = new ArrayList<>();
         suitesList.add(suite);
