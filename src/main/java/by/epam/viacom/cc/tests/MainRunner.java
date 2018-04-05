af9200413044e7da2c0c05667780e7e7ad6c4132
Denis_Galieuski <Denis_Galieuski@epam.com>
2018-04-05 15:39:00 +0300

    Excluded unnecessary methods (e.g. har file creation), optimized @BeforeClass methods

--- src/main/java/by/epam/viacom/cc/tests/MainRunner.java
+++ src/main/java/by/epam/viacom/cc/tests/MainRunner.java
@@ -3,28 +3,47 @@ package by.epam.viacom.cc.tests;
 
 import org.testng.TestNG;
 import org.testng.xml.SuiteXmlParser;
+import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainRunner {
     public static void main(String[] args) {
-        String file = "testng.xml";
-        SuiteXmlParser parser = new SuiteXmlParser();
-        XmlSuite suite = null;
-        try {
-            suite = parser.parse(file, new FileInputStream(file), true);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        String file = "src/resources/testng.xml";
+//        SuiteXmlParser parser = new SuiteXmlParser();
+//        XmlSuite suite = null;
+//        try {
+//            suite = parser.parse(file, new FileInputStream(file), true);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
-        List<XmlSuite> suitesList = new ArrayList<>();
-        suitesList.add(suite);
+
+        XmlSuite suite = new XmlSuite();
+        suite.setName("Suite1");
+
+        XmlTest test = new XmlTest(suite);
+        test.setName("Test1");
+        List<XmlClass> classes = new ArrayList<XmlClass>();
+
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("browser", "Firefox");
+        suite.setParameters(parameters);
+
+        classes.add(new XmlClass("by.epam.viacom.cc.tests.ProxyTest"));
+        test.setXmlClasses(classes);
+
+        List<XmlSuite> suites = new ArrayList<XmlSuite>();
+        suites.add(suite);
         TestNG tng = new TestNG();
-        tng.setXmlSuites(suitesList);
+        tng.setXmlSuites(suites);
         tng.run();
     }
 }
